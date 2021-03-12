d142a543b6ed51f7a197c3af727e62fccb57ba31
mmorrisonlk <mmorrisonlk@gmail.com>
2021-03-12 15:35:19 -0600

    Proof of concept working that adds time blocks with a for loop which should allow for easy scaling

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,14 @@ it sets the text content of currentDay to the Today variable in the format speci
 */
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+var hourNow = moment().format("h");
+console.log(hourNow);
+for (let index = 0; index < 5; index++) {
+    document.getElementById("hourBlocks").innerHTML += `
+    <div class="row descripton time-block">
+        <div class="col-2 border hour"> 9am </div>
+        <textarea class="col-8 border textarea past"></textarea>
+        <button class="col-2 border saveBtn">What about the wookies?</button>
+    </div>
+`;
+}
